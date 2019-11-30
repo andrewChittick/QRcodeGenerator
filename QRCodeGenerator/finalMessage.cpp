1c6d435d48ffbc0018953a70976d73f7783ac0b3
nickdonin <58269979+nickdonin@users.noreply.github.com>
2019-11-30 16:49:53 -0500

    Pushed

---
+++ QRCodeGenerator/finalMessage.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <iostream>
+#include <string>
+std::string makeFinalMessage(std::string* codewords, int* data, int numCode, int numErr);
+
+std::string makeFinalMessage(std::string* codewords, int* data, int numCode, int numErr)
+{
+	
+	std::string finalStr = "";
+	
+	for(int i = 0; i < numCode; i++)
+	{
+		finalStr += data[i];
+	}
+	for(int i = 0; i < numErr; i++)
+	{
+		finalStr += codewords[i];
+	}
+	if(numCode = 28)
+	{
+		finalStr += "0000000";
+	}
+	return finalStr;
+	
+
+}
