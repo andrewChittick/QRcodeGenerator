3e30d4342253a02cafe4f245c7fd5763e098bb60
nickdonin <58269979+nickdonin@users.noreply.github.com>
2019-11-30 16:59:47 -0500

    Header function included

--- QRCodeGenerator/buildMatrix.cpp
+++ QRCodeGenerator/buildMatrix.cpp
@@ -1,10 +1,7 @@
 #include <iostream>
-
+#include "matrixfun.h"
 using namespace std;
-int **makeMatrix(int *data, int version);
-void addFinder(int **matrix, int **patterns, int cornerx, int cornery);
-void setInfoAreas(int **matrix, int **patterns, int dimval);
-void layoutData(int **matrix, int **patterns, int *data, int dim, int ver);
+
 
 int **makeMatrix(int *data, int version)
 {
