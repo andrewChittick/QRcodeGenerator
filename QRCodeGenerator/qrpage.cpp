bfd39a6b270e818110fc44f5e0e3a3573fb221ee
Ben Zoiss <bzoiss@iu.edu>
2019-12-02 14:24:54 -0500

    a

--- QRCodeGenerator/qrpage.cpp
+++ QRCodeGenerator/qrpage.cpp
@@ -1,15 +1,12 @@
 #include "qrpage.h"
 #include "ui_qrpage.h"
 #include <iostream>
-#include <windows.h>
 
 QRPage::QRPage(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::QRPage)
 {
     ui->setupUi(this);
-    SetCurrentDirectoryA("..");
-    SetCurrentDirectoryA(".\\build-QRCodeGenerator-Desktop_Qt_5_12_6_MinGW_32_bit-Release");
     ui->label->setPixmap(QPixmap("QR.png"));
     ui->label->setScaledContents(true);
 
